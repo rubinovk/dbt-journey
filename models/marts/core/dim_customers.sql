76081290d671287842a3823a5daa34de79793b9d
Konstantin Rubinov <rubinov702@gmail.com>
2021-08-09 14:03:12 +0000

    lifetime_value

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -10,6 +10,13 @@ orders as (
 
 ),
 
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
 customer_orders as (
 
     select
@@ -17,9 +24,12 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(payments.amount) as lifetime_value
 
     from orders
+
+    left join payments using (order_id)
 
     group by 1
 
@@ -33,7 +43,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        customer_orders.lifetime_value
+
 
     from customers
 
