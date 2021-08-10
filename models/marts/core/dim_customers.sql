6a9154041a73caa5c6f56fa2d12e647698833fc5
Konstantin Rubinov <rubinov702@gmail.com>
2021-08-10 09:46:42 +0000

    checks

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -1,42 +1,20 @@
 with customers as (
-
     select * from {{ ref('stg_customers')}}
-
 ),
-
 orders as (
-
-    select * from {{ ref('stg_orders') }}
-
+    select * from {{ ref('fct_orders')}}
 ),
-
-
-payments as (
-
-    select * from {{ ref('stg_payments') }}
-
-),
-
 customer_orders as (
-
     select
         customer_id,
-
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(payments.amount) as lifetime_value
-
+        sum(amount) as lifetime_value
     from orders
-
-    left join payments using (order_id)
-
     group by 1
-
 ),
-
 final as (
-
     select
         customers.customer_id,
         customers.first_name,
@@ -45,12 +23,8 @@ final as (
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
         customer_orders.lifetime_value
-
-
     from customers
-
     left join customer_orders using (customer_id)
-
 )
-
 select * from final
+
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,7 +1,11 @@
 select
+    id as payment_id,
     orderid as order_id,
-    paymentmethod,
+    paymentmethod as payment_method,
     status,
-    amount
 
-from raw.stripe.payment
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw.stripe.payment 
